d6e67db13cee502da7ca5d26f431decd48cfcad7
Ramya <ramyasripathy2005@gmail.com>
2026-03-24 21:08:28 +0530

    debug db

--- db.php
+++ db.php
@@ -1,17 +1,13 @@
 <?php
 $url = getenv("DATABASE_URL");
 
+if (!$url) {
+    die("DATABASE_URL not found");
+}
+
+echo "URL FOUND<br>";
+
 $parts = parse_url($url);
 
-$host = $parts['host'];
-$user = $parts['user'];
-$pass = $parts['pass'];
-$db   = ltrim($parts['path'], '/');
-$port = $parts['port'];
-
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("DB ERROR: " . $conn->connect_error);
-}
-?>
+print_r($parts);
+exit();
